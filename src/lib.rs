c15996a681cc4f0dca1c624476b375508c63c438
Eva Pace <eba.pachi@gmail.com>
2023-07-06 09:12:27 -0300

    impl new

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,15 @@ pub struct MyVec<T> {
     len: usize,
 }
 
+impl<T> MyVec<T> {
+    pub fn new() -> Self {
+        MyVec {
+            ptr: NonNull::dangling(),
+            cap: 0,
+            len: 0,
+        }
+    }
+}
+
 unsafe impl<T: Send> Send for MyVec<T> {}
 unsafe impl<T: Sync> Sync for MyVec<T> {}
