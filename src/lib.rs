4e679dbea4f3e0855c9a62430c96894deca7ba11
Eva Pace <eba.pachi@gmail.com>
2023-07-06 09:14:47 -0300

    impl grow

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::alloc::{self, Layout};
 use std::ptr::NonNull;
 
 pub struct MyVec<T> {
@@ -13,6 +14,46 @@ impl<T> MyVec<T> {
             cap: 0,
             len: 0,
         }
+    }
+
+    fn grow(&mut self) {
+        let (new_cap, new_layout) = if self.cap == 0 {
+            (1, Layout::array::<T>(1).unwrap())
+        } else {
+            let new_cap = 2 * self.cap;
+
+            let new_layout = Layout::array::<T>(new_cap).unwrap();
+            (new_cap, new_layout)
+        };
+
+        assert!(
+            new_layout.size() <= isize::MAX as usize,
+            "allocation too large"
+        );
+
+        let new_ptr = if self.cap == 0 {
+            unsafe { alloc::alloc(new_layout) }
+        } else {
+            let old_layout = Layout::array::<T>(self.cap).unwrap();
+            let old_ptr = self.ptr.as_ptr() as *mut u8;
+            unsafe { alloc::realloc(old_ptr, old_layout, new_layout.size()) }
+        };
+
+        self.ptr = NonNull::new(new_ptr as *mut T)
+            .unwrap_or_else(|| alloc::handle_alloc_error(new_layout));
+
+        // self.ptr = match NonNull::new(new_ptr as *mut T) {
+        //     Some(p) => p,
+        //     None => alloc::handle_alloc_error(new_layout),
+        // };
+
+        // self.ptr = if !new_ptr.is_null() {
+        //     NonNull::new(new_ptr as *mut T).unwrap()
+        // } else {
+        //     alloc::handle_alloc_error(new_layout)
+        // };
+
+        self.cap = new_cap;
     }
 }
 
